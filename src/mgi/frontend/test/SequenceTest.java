52cf83b85debd3c2c169ae0f7a745f03ac74a328
Matt Hall <Matt.Hall@jax.org>
2010-07-30 12:11:13 +0000

    Initial version of the sequence files.

--- src/mgi/frontend/test/SequenceTest.java
+++ src/mgi/frontend/test/SequenceTest.java
@@ -3,7 +3,9 @@ package mgi.frontend.test;
 import java.util.Iterator;
 import java.util.List;
 
+import mgi.frontend.datamodel.Reference;
 import mgi.frontend.datamodel.Sequence;
+import mgi.frontend.datamodel.SequenceLocation;
 import mgi.persistence.HibernateUtil;
 import mgi.reporting.Timer;
 
@@ -17,13 +19,36 @@ public class SequenceTest {
 
 		Timer.write ("ready for initial query");
 		
-		List<Sequence> seqs = (List<Sequence>) session.createQuery ("from Sequence order by primaryID").setMaxResults(50).list();
+		List<Sequence> seqs = (List<Sequence>) session.createQuery ("from Sequence where primaryID = 'AK047243' order by primaryID").setMaxResults(50).list();
 		Timer.write ("retrieved " + seqs.size() + " sequence(s)");
 
 		Sequence seq;
 		for (Iterator iter = seqs.iterator(); iter.hasNext(); ) {
 			seq = (Sequence) iter.next();
-			System.out.println (seq.toString());
+			System.out.println("-------------------");
+			System.out.println(seq.getPrimaryID());
+			System.out.println(seq.getSequenceType());
+			System.out.println(seq.getLength());
+			System.out.println(seq.getStrain());
+			System.out.println(seq.getDescription());
+			System.out.println(seq.getProvider());
+			System.out.println(seq.getOrganism());
+			System.out.println(seq.getLibrary());
+			System.out.println(seq.getTissue());
+			
+          for (Iterator iter2 = seq.getReferences().iterator(); iter2.hasNext(); ) {
+              Reference mra = (Reference) iter2.next();
+              System.out.println (" --> " + mra.getTitle());
+              }
+          
+          for (Iterator iter2 = seq.getLocations().iterator(); iter2.hasNext(); ) {
+              SequenceLocation loc = (SequenceLocation) iter2.next();
+              System.out.println (" --> " + loc.getChromosome());
+              }
+			//System.out.println(seq.);  Chromosome stuff that isn't there yet.
+			//System.out.println(seq.get);
+			//System.out.println(seq.get);
+			System.out.println("-------------------");
 		}
 		Timer.write ("wrote sequence(s)");
 		
