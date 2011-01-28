9a49e53de14eda61700b3ebd2350908cbd4472ab
Matt Hall <Matt.Hall@jax.org>
2011-01-28 15:47:44 +0000

    *** empty log message ***

---
+++ src/mgi/frontend/datamodel/AnnotationInferredFromID.java
@@ -0,0 +1,79 @@
+package mgi.frontend.datamodel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="annotation_inferred_from_id")
+public class AnnotationInferredFromID {
+    
+    private String uniqueKey;
+    private String annotationKey;
+    private String logicalDB;
+    private String accID;
+    private Boolean isPreferred;
+    private Boolean isPrivate;
+    private String sequenceNum;
+    
+    @Id
+    @Column(name="unique_key")
+    public String getUniqueKey() {
+        return uniqueKey;
+    }
+    
+    @Column(name="annotation_key")
+    public String getAnnotationKey() {
+        return annotationKey;
+    }
+    
+    @Column(name="logical_db")
+    public String getLogicalDB() {
+        return logicalDB;
+    }
+    
+    @Column(name="acc_id")
+    public String getAccID() {
+        return accID;
+    }
+    
+    @Column(name="preferred")
+    public Boolean getIsPreferred() {
+        return isPreferred;
+    }
+    
+    @Column(name="private")
+    public Boolean getIsPrivate() {
+        return isPrivate;
+    }
+    
+    @Column(name="sequence_num")
+    public String getSequenceNum() {
+        return sequenceNum;
+    }
+    public void setUniqueKey(String uniqueKey) {
+        this.uniqueKey = uniqueKey;
+    }
+    public void setAnnotationKey(String annotationKey) {
+        this.annotationKey = annotationKey;
+    }
+    public void setLogicalDB(String logicalDB) {
+        this.logicalDB = logicalDB;
+    }
+    public void setAccID(String accID) {
+        this.accID = accID;
+    }
+    public void setIsPreferred(Boolean isPreferred) {
+        this.isPreferred = isPreferred;
+    }
+    public void setIsPrivate(Boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+    public void setSequenceNum(String sequenceNum) {
+        this.sequenceNum = sequenceNum;
+    }
+    
+    
+
+}
