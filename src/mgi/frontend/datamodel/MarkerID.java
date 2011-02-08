9f8b356ab891b3a568c32e091025b5869f672100
Jonathan Beal <Jon.Beal@jax.org>
2011-02-08 17:39:18 +0000

    added sequenceNum and isForOtherDbSection fields

--- src/mgi/frontend/datamodel/MarkerID.java
+++ src/mgi/frontend/datamodel/MarkerID.java
@@ -12,16 +12,36 @@ import javax.persistence.*;
 @Table (name="marker_id")
 public class MarkerID extends AccessionID {
 	private int markerKey;
+	private int sequenceNum;
+	private int isForOtherDbSection;
 
     // ================= Getters and Setters ===================== //
 	
+	@Column(name="is_for_other_db_section")
+	public int getIsForOtherDbSection() {
+		return isForOtherDbSection;
+	}
+
 	@Column(name="marker_key")
 	public int getMarkerKey() {
 		return markerKey;
 	}
+	
+	@Column(name="sequence_num")
+	public int getSequenceNum() {
+		return sequenceNum;
+	}
+
+	public void setIsForOtherDbSection(int isForOtherDbSection) {
+		this.isForOtherDbSection = isForOtherDbSection;
+	}
 
 	public void setMarkerKey(int markerKey) {
 		this.markerKey = markerKey;
+	}
+
+	public void setSequenceNum(int sequenceNum) {
+		this.sequenceNum = sequenceNum;
 	}
 
 	@Override
