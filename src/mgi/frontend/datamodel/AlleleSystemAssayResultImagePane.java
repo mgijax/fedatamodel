f94ad2ee5b6dd2800c5ef7f872e435b04aed071f
Jonathan Beal <Jon.Beal@jax.org>
2011-03-03 17:56:58 +0000

    fixed missed import

--- src/mgi/frontend/datamodel/AlleleSystemAssayResultImagePane.java
+++ src/mgi/frontend/datamodel/AlleleSystemAssayResultImagePane.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
