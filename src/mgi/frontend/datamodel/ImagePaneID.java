44ddc9625353ef76cbfee699215b50ade5a27dcf
Peter Frost <Pete.Frost@jax.org>
2012-02-20 12:51:53 +0000

    initial version

---
+++ src/mgi/frontend/datamodel/ImagePaneID.java
@@ -0,0 +1,33 @@
+package mgi.frontend.datamodel;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+/**
+ * ImagePaneID
+ */
+@Entity
+@Table (name="expression_imagepane_id")
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class ImagePaneID extends AccessionID {
+
+	private int imagepane_key;
+	private int sequenceNum;
+
+    // ================= Getters and Setters ===================== //
+
+
+	@Override
+	public String toString() {
+		return "ImagePaneID [imagepane_key=" + imagepane_key + ", "
+				+ (accID != null ? "accID=" + accID + ", " : "")
+				+ "isPreferred=" + isPreferred + ", isPrivate=" + isPrivate
+				+ ", "
+				+ (logicalDB != null ? "logicalDB=" + logicalDB + ", " : "")
+				+ "uniqueKey=" + uniqueKey + "]";
+	}
+}
