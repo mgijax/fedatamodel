33eb037e36eb82202373b2456c5220bb8ecb6271
Kevin Stone <kevinrstone@gmail.com>
2016-04-22 13:12:35 -0400

    adding QTL reference note

--- src/mgi/frontend/datamodel/Marker.java
+++ src/mgi/frontend/datamodel/Marker.java
@@ -8,7 +8,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.TreeMap;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
