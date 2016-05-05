298f7a7eabb77f19c22fad2a55509d994b90b6ac
Jonathan Beal <jsb@jax.org>
2016-05-05 16:07:19 -0400

    added to help support looking up terms by secondary IDs

---
+++ src/mgi/frontend/datamodel/VocabTermID.java
@@ -0,0 +1,41 @@
+package mgi.frontend.datamodel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * VocabTermID
+ * @author jsb
+ * A generic object to hold VocabTerm IDs (primary and secondary).
+ * It extends AccessionID, and inherits most of its methods from that base class.
+ */
+@Entity
+@Table (name="term_id")
+public class VocabTermID extends AccessionID {
+	private Integer termKey;
+	
+    // ================= Getters and Setters ===================== //
+
+	@Column(name="term_key")
+	public Integer getTermKey() {
+		return termKey;
+	}
+
+	public void setTermKey(Integer termKey) {
+		this.termKey = termKey;
+	}
+
+	@Override
+	public String toString() {
+		return "TermID ["
+				+ (accID != null ? "accID=" + accID + ", " : "")
+				+ "isPreferred=" + isPreferred + ", isPrivate=" + isPrivate
+				+ ", "
+				+ (logicalDB != null ? "logicalDB=" + logicalDB + ", " : "")
+				+ "uniqueKey=" + uniqueKey + "]";
+	}
+}
